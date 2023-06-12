ea91e15fd8f7c5bb01bae48ce91d0b58653436e5
clau-con-pan <133534427+pancon-jamon@users.noreply.github.com>
2023-06-12 11:31:53 -0500

    Primera version seri1 caracteres

---
+++ poliretos.c
@@ -0,0 +1,23 @@
+/*serie caarateres*/
+#include <stdio.h>
+//serie caracteres//
+void caracterAlternado()
+{
+    int n=0;
+    do
+    {
+        printf ("Elija un numero: ");
+        scanf ("%i",n);
+
+    } while (n>0)
+
+    for (int n>0; n++)
+    {
+        if (n%2==0); 
+        printf ("+");
+        else
+        printf ("-");
+    }
+
+
+}
